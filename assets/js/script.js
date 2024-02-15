8c0719051988b8680f0128da2332dcb20d6c6e92
ShantyLony <m.shanty@hotmail.be>
2024-02-15 23:29:00 +0100

    add  some assets: css and js

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+//  timeline projet
+
+
+
+"use strict";
+
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", DisplayContent);
+DisplayContent();
+
+function DisplayContent() {
+  const TriggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const topBox = box.getBoundingClientRect().top;
+    
+    if (topBox < TriggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
